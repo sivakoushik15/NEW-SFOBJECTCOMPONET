837091aaf00d191cd3f12be9ff46e60b4ac9f79c
sivakoushik <sivakoushik.d@autorabit.com>
2021-12-01 17:02:10 +0530

    added copy file

---
+++ demo - Copy.js
@@ -0,0 +1,46 @@
+function ValidateIPaddress(ip)
+{
+ if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip))
+  {
+   console.log('IP Address verified');
+  }
+  else
+  {
+   alert("You have entered an invalid IP address!")
+  }
+  if (true) {
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+  }
+  if (true) {
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+  }
+  if (true) {
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+  }
+  if(true){
+    console.log("sdggg");
+  }
+	
+	console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+	
+	system.out.println("");
+	system.out.println("");
+	system.out.println("");
+	system.out.println("");
+	system.out.println("");
+	system.out.println("");
+	system.out.println("");
+	system.out.println("");
+}
